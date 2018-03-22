259e4dd1883ffb9c8a667ab322eec041352777ee
Nobutarou <nobutarounosuke@gmail.com>
2018-03-23 01:09:28 +0900

    update

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # passpeco
-Pass with Peco
+pass with peco.
+
+pass is the great password manager.
+https://www.passwordstore.org/
+
+peco is the great interactive filterling tool.
+https://github.com/peco/peco
+
+pass + peco is the greatest password manager on console.
+
+## note
+
+For autotype and copy to clipboard, rofi-pass is the best of pass clients what I knwow.
+https://github.com/carnager/rofi-pass
+
+Therefore, I don't aim them. I only consider the usability on console.
+
+# 日本語訳
+
+pass と peco くっつけたら、すごく良さげなパスワード管理コンソールアプリになったよ
+自動記入とかコピペとかは rofi-pass っていうすごいのがあるから、そっち使って。
+
---
+++ passpeco.sh
@@ -0,0 +1,42 @@
+#!/bin/zsh
+
+# Maybe I should use PASSWORD_STORE_DIR.
+# Since I am not familier with \/'"{}()$, I cannot remove "$HOME/.password-store" smartly.
+# I ask you to re-write by yourself.
+
+SELECT=${$(find $HOME/.password-store -name "*.gpg" | sed 's/^\/home\/blabla\/.password-store\///' |peco --prompt="Entry> "):r}
+
+if [ -z $SELECT ];
+then exit;
+fi
+
+ACT=$(echo "show
+edit
+rm
+" | peco --prompt="Act> ")
+
+if [ -z $ACT ];
+then exit;
+fi
+
+# Your preference.
+export EDITOR=vim
+
+# show once to pass pinentry. For this, don't disable pinentry caching.
+pass show $SELECT 
+
+case $ACT in
+  show)
+    # I want smarter way. I want to get line number instead of the text itself. 
+    # Otherwise I cannot use "pass show -c".
+    pass show $SELECT | peco --prompt "Copy> " | xclip -r
+    ;;
+  edit)
+    pass edit $SELECT
+    ;;
+  rm)
+    pass rm $SELECT
+    ;;
+esac
+
+
